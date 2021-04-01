cf520e1818329c46dcd803d0ff8b99023fa5598e
Tak1za <varungupta2015135@gmail.com>
2021-04-01 20:46:10 +0530

    Added repository and created links to redirect to repo url

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import './Projects.scss';
-import jsonData from "../../data/repositories.json";
+import "./Projects.scss";
+import repositoryData from "../../data/repositories.json";
+import Link from "@material-ui/core/Link";
 
 function Projects() {
   const getData = () => {
-    var data = JSON.parse(JSON.stringify(jsonData));
+    var data = JSON.parse(JSON.stringify(repositoryData));
     console.log(data);
     setRepositories(data);
   };
@@ -18,7 +19,19 @@ function Projects() {
     <div className="Projects">
       {repositories &&
         repositories.length > 0 &&
-        repositories.map((repo) => <div key={repo.id}>({repo.title.toUpperCase()})</div>)}
+        repositories.map((repo) => (
+          <div key={repo.id}>
+            <Link
+              className="Repository"
+              target="_blank"
+              href={repo.url}
+              color="inherit"
+              rel="noreferrer"
+            >
+              ({repo.title.toUpperCase()})
+            </Link>
+          </div>
+        ))}
     </div>
   );
 }
