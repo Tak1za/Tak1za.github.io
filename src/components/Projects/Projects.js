19e4d18b54508f3e731b097a2902f2dba0c38283
Tak1za <varungupta2015135@gmail.com>
2021-04-01 21:05:57 +0530

    Moved json file to public folder

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./Projects.scss";
-// import repositoryData from "../../data/repositories.json";
 import Link from "@material-ui/core/Link";
 
 function Projects() {
   const getData = () => {
-    fetch("./repositories.json")
+    fetch(`${process.env.PUBLIC_URL}/data/repositories.json`)
       .then((res) => res.json())
-      .then((data) => setRepositories(data));
-    // var data = JSON.parse(JSON.stringify('./repositories.json'));
-    // console.log(data);
-    // setRepositories(data);
+      .then((data) => {
+        setRepositories(data);
+      });
   };
 
   const [repositories, setRepositories] = useState([]);
